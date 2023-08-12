b147cf5e447ea6cde0ab6f88b7be7164dc7ab6cd
Damien de Lartigue <damien@delartigue.co.uk>
2023-08-12 15:02:21 +0200

    add tests

---
+++ spec/rover_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+require 'spec_helper'
+require 'rspec'
+require_relative '../models/rover'
+require_relative '../models/mars_map'
+
+describe Rover do
+  describe "#move" do
+    it "moves the rover in the correct orientation" do
+      map = MarsMap.new(5, 5)
+      rover = Rover.new(3, 1, "N", map)
+      rover.move
+      expect(rover).to have_attributes(x_coord: 3, y_coord: 2)
+    end
+
+    it "stays within the bounds of the map" do
+      map = MarsMap.new(5, 5)
+      rover = Rover.new(0, 3, "W", map)
+      rover.move
+      expect(rover).to have_attributes(x_coord: 0, y_coord: 3)
+    end
+  end
+end
